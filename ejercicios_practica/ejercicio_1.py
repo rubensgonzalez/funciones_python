66d33e40f83ecbac5b55384a83b5747753a42e07
Ruben <87348517+rubensgonzalez@users.noreply.github.com>
2021-08-09 23:52:13 -0300

    Update ejercicio_1.py

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -11,6 +11,10 @@
 
 def imprimir_mayor(numero_1, numero_2):
     print("Funcion imprimir mayor")
+    if(numero_1 > numero_2):
+        print("El Mayor es: {}".format(numero_1))
+    else:
+        print("El Mayor es: {}".format(numero_2))
     # En esta función debe determinar cual de los dos
     # números ingresados por parámetro es mayor
     # y luego imprimir dicho valor en pantalla
