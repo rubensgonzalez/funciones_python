7d1a0892db874547dab69c09ccbc3ab507ade5fb
Ruben <87348517+rubensgonzalez@users.noreply.github.com>
2021-09-06 22:03:17 -0300

    Update ejercicio_3.py

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -13,6 +13,9 @@
 # Aquí dentro definir la función ordenar
 #def ordenar (numeros):
 
+def ordenar (numeros):
+    ordenado = numeros.sort()
+    return numeros
 
 # --------------------------------
 
@@ -23,7 +26,7 @@ if __name__ == '__main__':
     numeros = [2, 4, 10, 8, 12, 6]
 
     # Alumno: Crear la función "ordenar"
-
+   
     # Generar una una nueva funcion que se llame "ordenar",
     # que utilizaremos para odernar la lista de numeros.
     # Debe recibir 1 parámetro que es la lista de números
@@ -33,10 +36,12 @@ if __name__ == '__main__':
     # usando la funciones nativas de Python "sort"
 
     # Luego de crear la función invocarla en este lugar:
-
-    # lista_ordenada = ordenar(numeros)
+      
+    lista_ordenada = ordenar(numeros)
 
     # Imprimir en pantalla "lista_ordenada" que tendrá
     # los valores retornado por la función ordenar:
-
+    
+    print("\n Lista ordenada: {}".format(lista_ordenada))
+    
     print("terminamos")
